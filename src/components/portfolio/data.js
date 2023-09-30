250f6d8a9c98e55ac31ff33f6bc504840f327ecf
Sahil <sahifindmyphone@gmail.com>
2023-09-30 15:53:06 +0530

    new projects added

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -1,30 +1,45 @@
-import IMG1 from '../../assets/project_1.png'
-import IMG2 from '../../assets/project_2.png'
-import IMG3 from '../../assets/project_3.png'
+import TODO from '../../assets/todo.png'
+import KEEP from '../../assets/keep_note.png'
+import PORTFOLIO from '../../assets/portfolio.png'
+import ECOMMERCE from '../../assets/ecommerce.jpg'
+import CHAT from '../../assets/chat_room.jpg'
 // import IMG4 from '../../assets/portfolio4.jpg'
 // import IMG5 from '../../assets/portfolio5.png'
 // import IMG6 from '../../assets/portfolio6.jpg'
 export const data = [
     {
         id: 1,
-        Image: IMG1,
+        Image: ECOMMERCE,
+        title: "Ecommerce App",
+        github: "https://github.com/sahil974/mern-ecommerce",
+        demo: "https://my-shopp-app.netlify.app/"
+    },
+    {
+        id: 2,
+        Image: PORTFOLIO,
+        title: "Portfolio Website",
+        github: "https://github.com/sahil974/portfolio-wesite-1",
+        demo: "#home"
+    },
+    {
+        id: 3,
+        Image: TODO,
         title: "To DO List",
         github: "https://github.com/sahil974/todo-list",
         demo: "https://deft-sorbet-0c1219.netlify.app/"
     },
     {
-        id: 2,
-        Image: IMG2,
-        title: "Keep Notes",
-        github: "https://github.com/sahil974/Keep-note",
-        demo: "https://bright-salmiakki-a9ce61.netlify.app/"
+        id: 4,
+        Image: CHAT,
+        title: "Chat Room",
+        github: "https://github.com/sahil974/whatsapp",
+        demo: "https://chat-room-2.netlify.app/"
     },
     {
-        id: 3,
-        Image: IMG3,
-        title: "Portfolio Website",
-        github: "https://github.com/sahil974/portfolio-wesite-1",
-        demo: "#home"
+        id: 5,
+        Image: KEEP,
+        title: "Keep Notes",
+        github: "https://github.com/sahil974/Keep-note",
+        demo: "https://keep-note-assignment.netlify.app/"
     },
-
 ];
