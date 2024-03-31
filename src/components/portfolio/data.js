12cec3cd5daf17c42b6033ce1162d10addcf1cb5
Sahil <sahifindmyphone@gmail.com>
2024-03-31 21:04:31 +0530

    SaaS, password generator added and cv added

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -3,43 +3,57 @@ import KEEP from '../../assets/keep_note.png'
 import PORTFOLIO from '../../assets/portfolio.png'
 import ECOMMERCE from '../../assets/ecommerce.jpg'
 import CHAT from '../../assets/chat_room.jpg'
-// import IMG4 from '../../assets/portfolio4.jpg'
-// import IMG5 from '../../assets/portfolio5.png'
-// import IMG6 from '../../assets/portfolio6.jpg'
+import IMAGINOVA from '../../assets/IMAGINOVA2.jpg'
+import PASSWORD from '../../assets/password-generator.jpg'
+
 export const data = [
     {
         id: 1,
+        Image: IMAGINOVA,
+        title: "Imaginova App",
+        github: "https://github.com/sahil974/next-saas",
+        demo: "https://next-saas-gray.vercel.app/"
+    },
+    {
+        id: 2,
         Image: ECOMMERCE,
         title: "Ecommerce App",
         github: "https://github.com/sahil974/mern-ecommerce",
         demo: "https://testing-phase.netlify.app.netlify.app/"
     },
     {
-        id: 2,
+        id: 3,
         Image: PORTFOLIO,
         title: "Portfolio Website",
         github: "https://github.com/sahil974/portfolio-wesite-1",
         demo: "#home"
     },
     {
-        id: 3,
+        id: 4,
         Image: TODO,
         title: "To DO List",
         github: "https://github.com/sahil974/todo-list",
         demo: "https://my-app-notes.netlify.app/"
     },
     {
-        id: 4,
+        id: 5,
         Image: CHAT,
         title: "Chat Room",
         github: "https://github.com/sahil974/whatsapp",
         demo: "https://chat-room-2.netlify.app/"
     },
     {
-        id: 5,
+        id: 6,
         Image: KEEP,
         title: "Keep Notes",
         github: "https://github.com/sahil974/Keep-note",
         demo: "https://keep-note-assignment.netlify.app/"
+    },
+    {
+        id: 7,
+        Image: PASSWORD,
+        title: "Password Generator",
+        github: "https://github.com/sahil974/password-generator",
+        demo: "https://generate-your-passwordd.netlify.app/"
     },
 ];
